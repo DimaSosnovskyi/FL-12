bd974adfbb2de9b186d4cd74ea07a924e2429fc8
Dmytro Sosnovskyi <dimatuzinstagram@gmail.com>
2020-03-08 21:53:58 +0200

    HW7Full

--- FL-12_Webinar_HW7/webpack.config.js
+++ FL-12_Webinar_HW7/webpack.config.js
@@ -33,6 +33,11 @@ module.exports = {
         ]),
 
     ],
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 9000
+    },
     module: {
         rules: [
           {
